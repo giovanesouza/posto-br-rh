dc44977ae3e8f1d7427f61e4489a4bce2a075caf
Giovane Souza <developergiovanesouza@gmail.com>
2025-11-23 00:04:52 -0300

    refact: error 401 e 404

--- src/routes/ProctedRoute.jsx
+++ src/routes/ProctedRoute.jsx
@@ -1,15 +1,28 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import UnauthorizedImg from "../assets/error_401.png";
 
-export const ProtectedRoute = ({children}) => {
-    
-    const {userLogged, userInfo, isAdmin} = useContext(AuthContext); 
-    const allowedProfiles = ["employee", "manager", "admin"];
+export const ProtectedRoute = ({ children }) => {
+  const { userLogged, userInfo, isAdmin } = useContext(AuthContext);
+  const allowedProfiles = ["employee", "manager", "admin"];
 
-    if(!userLogged) return <Navigate to='/' />; 
-    
-    if(!isAdmin && !allowedProfiles.includes(userInfo.profile)) return <h1 className="text-align-center color-danger">! (401) Acesso negado!</h1>
+  console.log(userInfo.profile);
 
-    return children;
+  if (!userLogged) return <Navigate to="/" />;
+
+  if (!isAdmin && !allowedProfiles.includes(userInfo.profile)) {
+    return (
+      <div className="text-align-center color-danger">
+        <img
+          src={UnauthorizedImg}
+          style={{ maxWidth: "600px", width: "100%" }}
+          alt="Acesso não autorizado!"
+          title="Acesso não autorizado!"
+        />
+      </div>
+    );
+  }
+
+  return children;
 };
