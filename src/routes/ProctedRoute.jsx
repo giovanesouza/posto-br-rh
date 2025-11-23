c8dd5bdbd4045a1ba7ca5142ce8db90c416dedac
Giovane Souza <108561182+giovanesouza@users.noreply.github.com>
2025-11-23 00:22:59 -0300

    [REFACTOR] Login (#5)
    * fix: showToast method
    
    * refactor: authorization according to isAdmin and profile properties
    
    * refactor: Visibility of elements based on isAdmin property
    
    * refact: error 401 e 404

--- src/routes/ProctedRoute.jsx
+++ src/routes/ProctedRoute.jsx
@@ -1,13 +1,28 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import UnauthorizedImg from "../assets/error_401.png";
 
+export const ProtectedRoute = ({ children }) => {
+  const { userLogged, userInfo, isAdmin } = useContext(AuthContext);
+  const allowedProfiles = ["employee", "manager", "admin"];
 
-export const ProtectedRoute = ({children, IsAdmin}) => {
-    const {userLogged, isAdmin} = useContext(AuthContext); 
+  console.log(userInfo.profile);
 
-    if(!userLogged) return <Navigate to='/' />; 
-    if(!isAdmin) return <h1 className="text-align-center color-danger">! (401) Acesso negado!</h1>
+  if (!userLogged) return <Navigate to="/" />;
 
-    return children;
+  if (!isAdmin && !allowedProfiles.includes(userInfo.profile)) {
+    return (
+      <div className="text-align-center color-danger">
+        <img
+          src={UnauthorizedImg}
+          style={{ maxWidth: "600px", width: "100%" }}
+          alt="Acesso não autorizado!"
+          title="Acesso não autorizado!"
+        />
+      </div>
+    );
+  }
+
+  return children;
 };
