33d723f6299153bf4a23c702f49556d92c4b199e
Giovane Souza <developergiovanesouza@gmail.com>
2025-11-24 03:18:46 -0300

    test: add fixtures and commands to reuse of code

--- cypress/e2e/01_login.cy.js
+++ cypress/e2e/01_login.cy.js
@@ -1,40 +1,5 @@
 describe('Login', () => {
   beforeEach(() => {
-    // Intercept the POST /sign-in and return expected data for the application
-    cy.intercept('POST', '/sign-in', {
-      statusCode: 200,
-      body: {
-        employeeId: "897c63ed-680a-44b5-bcd2-7b442afc149b",
-        isAdmin: true,
-        profile: null,
-        token: "fake-token",
-        userId: "f9fce246-a363-4f00-bfd2-9aed6378d771"
-      }
-    }).as('mockLoginAdmin');
-
-    // Intercept the GET /employees and return a mocked list
-    cy.intercept('GET', '/employees', (req) => {
-      expect(req.headers.authorization).to.eq('Bearer fake-token');
-      req.reply({
-        statusCode: 200,
-        body: [
-          { name: "João Silva", position: "Analista" },
-          { name: "Maria Souza", position: "Gerente" }
-        ]
-      });
-    }).as('mockEmployees');
-
-    // Simulate complete UserInfo in localStorage
-    cy.window().then((win) => {
-      win.localStorage.setItem('userInfo', JSON.stringify({
-        employeeId: "897c63ed-680a-44b5-bcd2-7b442afc149b",
-        isAdmin: true,
-        profile: null,
-        token: "fake-token",
-        userId: "f9fce246-a363-4f00-bfd2-9aed6378d771"
-      }));
-    });
-
     cy.visit('/');
   });
 
@@ -46,65 +11,121 @@ describe('Login', () => {
 
   it('2. Should show error when submitting empty fields', () => {
     cy.get('button[type=submit]').click();
+    cy.wait(2000); // wait to visually confirm the error message
     cy.get('div').should('contain', 'Verifique se os campos foram preenchidos corretamente');
   });
 
   it('3. Invalid data: Should display error message', () => {
     cy.login('usuario_teste', '123456');
+    cy.wait(2000); // wait to visually confirm the error message
     cy.get('div').should('contain', 'Verifique se os campos foram preenchidos corretamente');
   });
 
-  it('4. Admin User should authenticate with valid credentials', () => {
-    cy.login('giovanesouza', '123456');
-    cy.wait('@mockLoginAdmin');
-    cy.url().should('include', '/app/funcionarios');
-    cy.get('ul.menu-hidden').should('exist').and('be.visible');
-    cy.get('button.material-symbols-outlined').should('exist').and('be.visible');
-    cy.get('button.logout').should('exist').and('be.visible');
-    cy.get('h1').should('have.text', 'Situação dos funcionários');
-    cy.get('input[name="searchEmployeeName"').should('exist').and('be.visible');
-    cy.get('table').should('exist').and('be.visible');
-    cy.get('body').should('contain', 'Nenhum funcionário cadastrado!');
-  });
-
-  it('5. Regular employee user should authenticate and be redirected to vacation history', () => {
-    // Intercept login as regular employee
-    cy.intercept('POST', '/sign-in', {
-      statusCode: 200,
-      body: {
-        employeeId: "897c63ed-680a-44b5-bcd2-7b442afc149b",
-        isAdmin: false,
-        profile: "employee",
-        token: "fake-token",
-        userId: "f9fce246-a363-4f00-bfd2-9aed6378d771"
-      }
-    }).as('mockLoginEmployee');
-
-    // Simulate UserInfo in localStorage
-    cy.window().then((win) => {
-      win.localStorage.setItem('userInfo', JSON.stringify({
-        employeeId: "897c63ed-680a-44b5-bcd2-7b442afc149b",
-        isAdmin: false,
-        profile: "employee",
-        token: "fake-token",
-        userId: "f9fce246-a363-4f00-bfd2-9aed6378d771"
-      }));
-    });
-
-    cy.visit('/');
+  it('4. Admin should authenticate and view an empty employees list', () => {
+    // Intercept login as admin user
+    cy.mockAthenticateAs('adminUserLocalStorage'); // uses the custom command to mock login as admin
+    cy.getEmployees(); // when without parameter, return an empty employees list
 
     // Perform login
     cy.login('giovanesouza', '123456');
-    cy.wait('@mockLoginEmployee');
+    cy.wait('@mockSignInRequest'); // wait for the mocked sign-in request
+    cy.wait('@mockEmployees'); // wait for the mocked employees request
 
     // Verify redirection to the correct route
-    cy.url().should('include', '/app/historico-de-ferias/funcionario/897c63ed-680a-44b5-bcd2-7b442afc149b');
+    cy.url().should('include', '/app/funcionarios');
+    cy.wait(2000); // wait to visually success message
+    cy.get('div').should('contain', 'Olá, Admin. Seja bem vindo(a)!');
+    cy.get('button.Toastify__close-button').click();
+    cy.wait(2000);
+
+    cy.get('ul.menu-hidden').should('exist').and('be.visible');
+    cy.get('button.material-symbols-outlined[title="Configurações do Admin"]').should('exist').and('be.visible');
+    cy.get('button.logout').should('exist').and('be.visible');
+    cy.get('h1').should('have.text', 'Situação dos funcionários'); // verify exact text
+    cy.get('input[name="searchEmployeeName"').should('exist').and('be.visible');
+    cy.get('table').should('exist').and('be.visible');
+    cy.get('body').should('contain', 'Nenhum funcionário cadastrado!');
+    cy.get('footer address').should('exist').and('be.visible');
+  });
+
+    it('5. Admin should authenticate and view the employees list upon login', () => {
+    // Intercept login as admin user
+    cy.mockAthenticateAs('adminUserLocalStorage'); // uses the custom command to mock login as admin
+    cy.getEmployees('employees'); // uses the custom command to mock employees data; 
+
+    // Perform login
+    cy.login('giovanesouza', '123456');
+    cy.wait('@mockSignInRequest'); // wait for the mocked sign-in request
+    cy.wait('@mockEmployees'); // wait for the mocked employees request
+
+    // Verify redirection to the correct route
+    cy.url().should('include', '/app/funcionarios');
+    cy.wait(2000); // wait to visually success message
+    cy.get('div').should('contain', 'Olá, Admin. Seja bem vindo(a)!');
+    cy.get('button.Toastify__close-button').click();
+    cy.wait(2000);
+    
+    cy.get('ul.menu-hidden').should('exist').and('be.visible');
+    cy.get('button.material-symbols-outlined[title="Configurações do Admin"]').should('exist').and('be.visible');
+    cy.get('button.logout').should('exist').and('be.visible');
+    cy.get('h1').should('have.text', 'Situação dos funcionários'); // verify exact text
+    cy.get('input[name="searchEmployeeName"').should('exist').and('be.visible');
+    cy.get('table').should('exist').and('be.visible');
+    cy.get('table .employee-situation.has-vacation').should('exist').and('be.visible').and('contain', 'Direito a férias');
+    cy.get('table .employee-situation.no-vacation').should('exist').and('be.visible').and('contain', 'Sem direito a férias');
+    cy.get('button.bg-hover-green.color-green').should('exist').and('be.visible');
+    cy.get('button.bg-hover-blue.color-blue').should('exist').and('be.visible');
+    cy.get('button.bg-hover-danger.color-danger').should('exist').and('be.visible');
+    cy.get('footer address').should('exist').and('be.visible');
+  });
+
+  it('6. Employee should authenticate and be redirected to an empty vacation history', () => {
+    const employeeId = "897c63ed-680a-44b5-bcd2-7b442afc149b"; // ID of the employee in the fixture
+
+    // Intercept login as regular employee
+    cy.mockAthenticateAs('employeeUserLocalStorage');
+    cy.getEmployeeById(employeeId); // uses the custom command to mock employee by ID data
+
+    // Perform login
+    cy.login('pedrohenrique', '123456');
+    cy.wait('@mockSignInRequest'); // wait for the mocked sign-in request
+    
+    // Verify redirection to the correct route
+    cy.url().should('include', `/app/historico-de-ferias/funcionario/${employeeId}`);
+    cy.wait('@mockEmployeeById'); // wait for the mocked employee by ID request
 
     cy.get('ul.menu-hidden').should('not.exist');
     cy.get('div.right-menu').should('exist').and('be.visible');
     cy.get('button.logout').should('exist').and('be.visible');
-    cy.get('h1').should('contain', 'Histórico de férias do(a) funcionário(a):');
+    cy.get('h1.text-align-center').should('contain', 'Histórico de férias do(a) funcionário(a): ').and('contain', 'Pedro Henrique Ferreira Silva'); // verify partial text
     cy.get('table').should('exist').and('be.visible');
-    cy.get('body').should('contain', 'Nenhuma férias cadastrada!');
+    cy.get('table td').should('contain', 'Nenhuma férias cadastrada!');
+    cy.get('footer address').should('exist').and('be.visible');
   });
+
+
+  it('7. Employee should authenticate and view vacation history', () => {
+    const employeeId = "987c63ed-670a-44b5-bcd2-7b452afc148b"; 
+
+    // Intercept login as regular employee
+    cy.mockAthenticateAs('employeeUserWithVacationLocalStorage');
+    cy.getEmployeeById(employeeId); // uses the custom command to mock employee by ID data
+
+    // Perform login
+    cy.login('mariaoliveira', '123456');
+    cy.wait('@mockSignInRequest'); // wait for the mocked sign-in request
+    
+    // Verify redirection to the correct route
+    cy.url().should('include', `/app/historico-de-ferias/funcionario/${employeeId}`);
+    cy.wait('@mockEmployeeById'); // wait for the mocked employee by ID request
+
+    cy.get('ul.menu-hidden').should('not.exist');
+    cy.get('div.right-menu').should('exist').and('be.visible');
+    cy.get('button.logout').should('exist').and('be.visible');
+    cy.get('h1.text-align-center').should('contain', 'Histórico de férias do(a) funcionário(a): ').and('contain', 'Maria Clara Souza Oliveira'); // verify partial text
+    cy.get('table').should('exist').and('be.visible');
+    cy.get('table td').should('not.contain', 'Nenhuma férias cadastrada!');
+    cy.get('footer address').should('exist').and('be.visible');
+  });
+
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,14 +24,90 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('login', (username, password) => { 
-    cy.get('input[name=username]').type(username);
-    cy.get('input[name=password]').type(password);
-    cy.get('button[type=submit]').click();
+// Login: The username must be between 12 to 20 letters and password, 6 to 20.
+Cypress.Commands.add('login', (username, password) => {
+  cy.get('input[name=username]').type(username);
+  cy.get('input[name=password]').type(password);
+  cy.get('button[type=submit]').click();
 });
 
 Cypress.Commands.add('clearAllInputs', () => {
   cy.get('input').each(($el) => {
     cy.wrap($el).clear();
+  });
+});
+
+
+
+
+// ============ Mocks ============ 
+
+// Login for different users
+
+Cypress.Commands.add('mockAthenticateAs', (userFixture) => {
+  cy.fixture(userFixture).then((userInfo) => {
+    cy.intercept('POST', '/sign-in', {
+      statusCode: 200,
+      body: userInfo
+    }).as('mockSignInRequest');
+
+    cy.window().then((win) => {
+      win.localStorage.setItem('userInfo', JSON.stringify(userInfo));
+    });
+  });
+});
+
+
+// Mock Employees data - Intercept the GET '/employees' and return a mocked list
+Cypress.Commands.add('getEmployees', (fixture) => {
+  if (!fixture) {
+    // Nenhum parâmetro → retorna lista vazia
+    cy.intercept('GET', '/employees?*', (req) => {
+      expect(req.headers.authorization).to.eq('Bearer fake-token'); // validate auth header is present and correct
+      req.reply({
+        statusCode: 200,
+        body: []
+      });
+    }).as('mockEmployees');
+  } else {
+    cy.fixture(fixture).then((employees) => {
+      cy.intercept('GET', '/employees?*', (req) => {
+        expect(req.headers.authorization).to.eq('Bearer fake-token');
+        // Reply with mocked employees data
+        req.reply({
+          statusCode: 200,
+          body: employees
+        });
+      }).as('mockEmployees');
+    });
+  }
+});
+
+
+Cypress.Commands.add('getEmployeeById', (id) => {
+  cy.intercept('GET', `/employees/${id}`, (req) => {
+    expect(req.headers.authorization).to.eq('Bearer fake-token');
+
+
+    req.reply({
+      statusCode: 200,
+      body: employeeData
+    });
+  }).as('mockEmployeeById');
+});
+
+Cypress.Commands.add('getEmployeeById', (employeeId, fixture = 'employees') => {
+  cy.fixture(fixture).then((employees) => {
+    // Find the employee with the matching ID in the fixture data
+    const employee = employees.find(e => e.id === employeeId);
+
+    cy.intercept('GET', `/employees/${employeeId}`, (req) => {
+      expect(req.headers.authorization).to.eq('Bearer fake-token');
+
+      req.reply({
+        statusCode: 200,
+        body: employee || {} // return empty object if not found
+      });
+    }).as('mockEmployeeById');
   });
 });
