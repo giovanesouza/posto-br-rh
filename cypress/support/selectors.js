33413b6bd1376ea59614ae49ab7da3267917d1d5
Giovane Souza <developergiovanesouza@gmail.com>
2025-11-30 01:39:27 -0300

    feat: mapping elements related to login and employees

---
+++ cypress/support/selectors.js
@@ -0,0 +1,64 @@
+export const selectors = {
+  // ====== AUTH ======
+  login: {
+    username: 'input[name="username"]',
+    password: 'input[name="password"]',
+    submitBtn: 'button[type="submit"]'
+  },
+
+  // ====== INPUTS ======
+  inputs: {
+    allInputs: 'input',
+    searchEmployeeName: 'input[name="searchEmployeeName"]',
+    employeeName: '#employeeName',
+    cpf: '#cpf',
+    admissionDate: '#admissionDate'
+  },
+
+  // ====== BUTTONS ======
+  buttons: {
+    submit: 'button[type="submit"]',
+    logout: 'button.logout',
+    adminSettings: 'button.material-symbols-outlined[title="Configurações do Admin"]',
+
+    // employee list actions
+    viewBtn: 'button.bg-hover-blue.color-blue',
+    editBtn: 'button.bg-hover-green.color-green',
+    deleteBtn: 'button.bg-hover-danger.color-danger'
+  },
+
+  // ====== MENU ======
+  menu: {
+    sidebar: 'ul.menu-hidden',
+    rightMenu: 'div.right-menu',
+    listEmployees: 'a[href="/app/funcionarios"]',
+    createEmployee: 'a[href="/app/cadastrar-funcionario"]',
+    createUser: 'a[href="/app/cadastrar-usuario"]'
+  },
+
+  // ====== FOOTER ======
+  footer: {
+    address: 'footer address'
+  },
+
+  // ====== HEADERS ======
+  headers: {
+    pageTitle: 'h1'
+  },
+
+  // ====== EMPLOYEE LIST PAGE ======
+  employeeList: {
+    table: 'table',
+    tableRows: 'table tbody tr',
+    situationHasVacation: 'table .employee-situation.has-vacation',
+    situationNoVacation: 'table .employee-situation.no-vacation',
+    emptyMessage: 'table td'
+  },
+
+  // ====== TOASTS ======
+  toasts: {
+    success: '.Toastify__toast--success[role="alert"]',
+    error: '.Toastify__toast--error[role="alert"]',
+    closeBtn: 'button.Toastify__close-button'
+  }
+};
