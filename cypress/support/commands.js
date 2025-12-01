bfb19c35c61ab899c5d763d31823b3df37d0cbcc
Giovane Souza <developergiovanesouza@gmail.com>
2025-12-01 00:36:51 -0300

    fix: add scrollToTop

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -68,15 +68,15 @@ Cypress.Commands.add('fillInput', (selector, value) => cy.get(selector).should('
 // =========================
 
 Cypress.Commands.add('verifyErrorToast', (text) => {
+  cy.scrollTo('top');
   cy.get(selectors.toasts.error, { timeout: 8000 })
-    .scrollIntoView()
     .should('be.visible')
     .and('contain.text', text)
 });
 
 Cypress.Commands.add('verifySuccessToast', (text) => {
+  cy.scrollTo('top');
   cy.get(selectors.toasts.success, { timeout: 8000 })
-    .scrollIntoView()
     .should('be.visible')
     .and('contain.text', text)
 });
